bb2f96f03a0344309aa3fa30bfa05af5dcf5b38b
Nicklas Storm Jensen <Nist@netcompany.com>
2017-08-11 15:46:29 +0200

    Alt muligt?

---
+++ FriendZone/app/src/main/java/nist/friendzone/SignupNameFragment.java
@@ -0,0 +1,18 @@
+package nist.friendzone;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignupNameFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup_name, container, false);
+    }
+}
