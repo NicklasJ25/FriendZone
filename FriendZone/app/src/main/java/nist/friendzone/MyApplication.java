1a7fcf18baea32563421d6ee4cd9b821523ea7ef
Nickas Storm Jensen <Nist@netcompany.com>
2017-10-20 07:24:06 +0200

    Tilføjelse af CommentController

--- FriendZone/app/src/main/java/nist/friendzone/MyApplication.java
+++ FriendZone/app/src/main/java/nist/friendzone/MyApplication.java
@@ -6,7 +6,8 @@ import io.realm.Realm;
 
 public class MyApplication extends Application
 {
-    public static String baseUrl = "http://10.216.10.130/FriendZone/api/"; //Hjemme
+    //public static String baseUrl = "http://10.216.10.130/FriendZone/api/"; //Hjemme
+    public static String baseUrl = "http://10.132.206.131/FriendZone/api/"; //Netcompany cellular
     //public static String baseUrl = "http://0.0.0.0/FriendZone/api/"; //NNE
 
     @Override
